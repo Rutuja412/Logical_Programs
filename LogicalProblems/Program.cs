c729894a93c5be98a5c96a55274b8ff36ecb062b
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-11 21:14:39 +0530

    [Rutuja]:Adding Fibonacci Series Program

---
+++ LogicalProblems/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace LogicalProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Logical Problems");
+            Console.WriteLine("Fibonacci Series");
+            FibonacciSeries fibonacciSeries = new FibonacciSeries();
+            fibonacciSeries.series();
+        }
+    }
+}
